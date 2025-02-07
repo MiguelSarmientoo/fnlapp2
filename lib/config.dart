652926675bc35bb4847010ca485cf4e8413c7819
Juan Gutierrez <juan089207@gmail.com>
2025-02-07 01:12:53 -0500

    config dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,4 @@
 class Config {
-  static const String apiUrl = 'http://18.228.80.10:3000/api';
-  static const String imagenesUrl = 'http://18.228.80.10:3000/api';
+  static const String apiUrl = 'http://localhost:3000/api';
+  static const String imagenesUrl = 'http://localhost:3000/api';
 }

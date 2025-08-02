d2aa13ab1df715b141cf4ba3e2ab52ec5b475733
Ghotofc99 <juan089207@gmail.com>
2025-08-01 23:20:54 -0500

    ultimos cambios

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:fnlapp/Login/login.dart';
 import 'package:fnlapp/Preguntas/index.dart';
 import 'package:fnlapp/Main/cargarprograma.dart';
@@ -6,11 +7,22 @@ import 'package:fnlapp/Main/home.dart';
 import '../Util/api_service.dart';
 import 'package:fnlapp/SplashScreen/splashscreen.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
-
+import 'package:fnlapp/route_decider.dart';
 
 
 void main() {
-  usePathUrlStrategy(); 
+  WidgetsFlutterBinding.ensureInitialized();
+  usePathUrlStrategy();
+
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    systemNavigationBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.dark,
+    systemNavigationBarIconBrightness: Brightness.dark,
+  ));
+
   runApp(const MyApp());
 }
 
@@ -20,8 +32,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final ApiService apiService = ApiService();
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Funcy FNL',
@@ -30,18 +40,24 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       // Define tus rutas
-      initialRoute: '/',
-      // Definimos todas las rutas de forma estática
-      routes: {
-        '/': (context) => const SplashScreen(),
-        '/login': (context) => LoginScreen(),
-        '/home': (context) => HomeScreen(),
-        // IndexScreen necesita parámetros, así que lo manejamos de esta forma
-        '/index': (context) => IndexScreen(
-          username: 'username', // Este dato debería venir de SharedPreferences
-          apiServiceWithToken: apiService,
-        ),
-      },
+      initialRoute: '/splash',
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/':
+          case '/splash':
+            return MaterialPageRoute(builder: (_) => SplashScreen());
+          case '/login':
+            return MaterialPageRoute(builder: (_) => LoginScreen());
+          case '/home':
+          case '/index':
+            return MaterialPageRoute(
+              builder: (_) => RouteWrapper(routeName: settings.name ?? '/'),
+              settings: settings,
+            );
+          default:
+            return MaterialPageRoute(builder: (_) => SplashScreen());
+        }
+      }
     );
   }
 }
